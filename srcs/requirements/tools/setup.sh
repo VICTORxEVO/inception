5bd3c065e00e9366f556322be1449be419770dc6
ViktorEvo <yassirsbai04@gmail.com>
2026-02-06 11:57:31 +0100

    fix: remove nginx_logs and fail2ban directories from setup script

--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -2,7 +2,7 @@
 
 LOGIN="$(grep '^LOGIN=' ./srcs/.env | cut -d '=' -f2)"
 
-mkdir -p "/home/${LOGIN}/data/db" "/home/${LOGIN}/data/wp" "/home/${LOGIN}/data/nginx_logs" "/home/${LOGIN}/data/fail2ban"
+mkdir -p "/home/${LOGIN}/data/db" "/home/${LOGIN}/data/wp"
 
 
 
